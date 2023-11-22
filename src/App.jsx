aff9e832d1645473d5cc66063576864f03c36c73
Emanuele Colabello <emanuele.colabello@gmail.com>
2023-11-22 11:17:35 +0100

    src/App.jsx: refactor App component to use markdown editor and preview.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { marked } from 'marked';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import WindowBox from './components';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [markdown, setMarkdown] = React.useState('');
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main id="markdown-app">
+      <WindowBox title="Editor" icon={<FontAwesomeIcon icon={faEdit} />}>
+        <textarea id="editor" className="editor" value={markdown} onChange={(e) => setMarkdown(e.target.value)} />
+      </WindowBox>
+      <WindowBox title="Preview" width={1000} icon={<FontAwesomeIcon icon={faEdit} />}>
+        <div id="preview" dangerouslySetInnerHTML={{ __html: marked(markdown) }} /> {/* eslint-disable-line react/no-danger */}
+      </WindowBox>
+    </main>
+  );
 }
-
-export default App
+export default App;
